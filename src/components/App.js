d34061ae90b44262cf6b3136d3653b7df97f8cdb
Roman <maksudov.rr@yandex.ru>
2024-03-28 11:29:30 +0300

    1.3

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,14 @@ import React, { Component, Fragment } from "react";
 
 import "../styles/App.css";
 import Header from "./Header"
+import Main from "./Main";
 
 class App extends Component {
     render() {
         return (
             <Fragment>
             <Header />
-            <main>
-                <div>
-                    <h1>Weather</h1>
-                </div>
-            </main>
+            <Main />
             </Fragment>
         );
     }
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import "../styles/Main.css";
+
+class Main extends Component {
+    render() {
+        return (
+            <main>
+                <div>
+                    <h1>Weather</h1>
+                </div>
+            </main>
+        )
+    }
+}
+
+export default Main;
