edad242f4ef8796d9970472b4ff31bb7a305a5d4
Roman <maksudov.rr@yandex.ru>
2024-03-28 11:20:05 +0300

    1.2

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import "../styles/App.css";
 import Header from "./Header"
@@ -6,14 +6,14 @@ import Header from "./Header"
 class App extends Component {
     render() {
         return (
-            <React.Fragment>
+            <Fragment>
             <Header />
             <main>
                 <div>
                     <h1>Weather</h1>
                 </div>
             </main>
-            </React.Fragment>
+            </Fragment>
         );
     }
 }
