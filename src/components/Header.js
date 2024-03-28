2d0cfda258487173a75e6d507aec8d84991dc6c9
Roman <maksudov.rr@yandex.ru>
2024-03-28 14:35:05 +0300

    1.5

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,8 @@ import React, { Component, useState } from "react";
 import "../styles/Header.css";
 
 function Header() {
-    let buttonName = "Select City";
-    let count = 0;
-    let [count, setAttribute] = useState(a:0)
-    const handleClick = () => {
-        console.log('your city');
-    }
-
         return (
-            <header>Weather App
-                <button className={ "select-city" } onClick={ handleClick }>{buttonName}</button>
-            </header>
+            <header>Weather App</header>
         )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,35 @@ class Main extends Component {
     render() {
         return (
             <main>
-                <div>
-                    <h1>Weather</h1>
+                <div className={ "container" }>
+                    <div className={ "search-box" }>
+                        <i className={ "location-dot" }></i>
+                        <input type="text" placeholder="Enter your city"></input>
+                        <button className={ "submit" }></button>
+                    </div>
+                    <div className={ "weather" }>
+                        <div className={ "weather-image" }>
+                            <i className={ "cloud" }></i>
+                        </div>
+                        <h1 className={ "temp" }>22 &#8451</h1>
+                        <h2 className={ "city" }>Moscow</h2>
+                        <div className={"details"}>
+                            <div className={"col"}>
+                                <i className={"water"}></i>
+                                <div>
+                                    <p className={"humidity"}>50%</p>
+                                    <p>Humidity</p>
+                                </div>
+                            </div>
+                            <div className={"col"}>
+                                <i className={"wind"}></i>
+                                <div>
+                                    <p className={"humidity"}>15 km/h</p>
+                                    <p>Wind speed</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         )
