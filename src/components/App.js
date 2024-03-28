2074f3eba2235fb0ab510301f4af9387bf3c4ea5
Roman <maksudov.rr@yandex.ru>
2024-03-28 11:08:39 +0300

    1.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 import "../styles/App.css";
+import Header from "./Header"
 
 class App extends Component {
     render() {
         return (
             <React.Fragment>
-            <header>Weather App</header>
+            <Header />
             <main>
                 <div>
                     <h1>Weather</h1>
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Comment } from "react";
+
+import "../styles/Header.css";
+
+class Header extends Component {
+    render() {
+        return (
+            <header>Weather App</header>
+        )
+    }
+}
+
+export default Header;
