7a717ee1af90b4f28a1704b8627b73c80ce150f7
Roman <maksudov.rr@yandex.ru>
2024-03-28 16:15:19 +0300

    1.7

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,34 @@ import React, { Component } from "react";
 
 import "../styles/Main.css";
 
-class Main extends Component {
-    render() {
-        return (
+function Main(){
+
+    const apiKey = "366d1896809eed5516d485434ad6f3e2";
+
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+    const searchInput = document.querySelector(".search-box input");
+    const searchButton = document.querySelector(".search-box button");
+
+    async function checkWeather(city) {
+    const responce = await fetch(apiUrl + city + '&appid={apiKey}');
+    const data = await responce.json();
+    console.log(data, "data");
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+        Math.round(data.main.temp) + '&#8451';
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    }
+
+
+    searchButton.addEventListener("click", () => {
+    checkWeather(searchInput.value);
+    searchInput.value = "";
+    })    
+
+    return (
             <main>
                 <div className={ "container" }>
                     <div className={ "search-box" }>
@@ -39,6 +64,5 @@ class Main extends Component {
             </main>
         )
     }
-}
 
 export default Main;
